873856f596b565a13f9fda50346c25f1ccd036c6
David Bolin <david@bolin.at>
2019-03-28 08:40:59 -0700

    fixed a navigation problem

--- src/Folder.js
+++ src/Folder.js
@@ -9,13 +9,13 @@ class Folder extends React.Component  {
   static contextType = NotefulContext;
   
   render(){
-    let cname;
-    if (this.props.match.params.id === this.props.folderid) {
+    let cname = 'folderItem';
+    if (this.props.match) {
+      if (this.props.match.params.id === this.props.folderid) {
       cname = 'folderItem selected';
-    } else {
-      cname = 'folderItem';
+      }
     }
-    
+
     return ( 
       <li key={this.props.folderid} className ={cname}>         
         <NavLink onClick={() => this.context.changeOrigin(false)} to={`/Folder/${this.props.folderid}`}>

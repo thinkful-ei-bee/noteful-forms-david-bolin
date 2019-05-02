b9699f19976a9fde3e10214a6290b9115f6a8c15
David Bolin <david@bolin.at>
2019-05-02 15:40:44 -0700

    modified folderlist to avoid undefined problem

--- src/FolderList.js
+++ src/FolderList.js
@@ -8,12 +8,17 @@ class FolderList extends React.Component  {
   static contextType = NotefulContext;
 
   render() {
-    let folders = this.context.store.folders.map(folder =>{
-      return <Folder match={this.props.match}
-        key={folder.id}
-        folderid={folder.id} 
-        folderName= {folder.name} />
-    })
+    let folders;
+    if (this.context.store.folders) {
+      folders = this.context.store.folders.map(folder =>{
+        return <Folder match={this.props.match}
+          key={folder.id}
+          folderid={folder.id} 
+          folderName= {folder.name} />
+      })
+    } else {
+      folders = [];
+    }
       
     return (
       <div className='leftBar'>
